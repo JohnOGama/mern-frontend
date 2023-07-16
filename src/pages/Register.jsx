741c36e1fc9de93028fd7c7c32843ba916be3429
John De Leon <112483991+JohnOGama@users.noreply.github.com>
2023-07-16 23:16:24 +0800

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,7 +8,7 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const register = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:4000/register", {
+    const response = await fetch("https://mern-backend-api-seven.vercel.app/register", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
