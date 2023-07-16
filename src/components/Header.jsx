edda75c8ef5dacfaf4a93feebf6327aa8da782fb
John De Leon <112483991+JohnOGama@users.noreply.github.com>
2023-07-16 23:17:15 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { UserContext } from "../UserContext";
 const Header = () => {
   const { setUserInfo, userInfo } = useContext(UserContext);
   useEffect(() => {
-    fetch("http://localhost:4000/profile", {
+    fetch("https://mern-backend-api-seven.vercel.app/profile", {
       credentials: "include",
     })
       .then((response) => response.json())
